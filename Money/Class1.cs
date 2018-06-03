a0b978c63649e976a39a0385ec29926c094d1557
vika55555 <v_kalvin@ukr.net>
2018-06-03 15:11:16 +0300

    Added Money.dll

---
+++ Money/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Class1
+    {
+        public int money;
+
+        public Class1(int m = 0)
+        {
+            money = m;
+        }
+
+        public void Add(int m)
+        {
+            money += m;
+        }
+
+        public void Remove(int m)
+        {
+            money -= m;
+        }
+    }
+}
--- Purse/Program.cs
+++ Purse/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Money;
 
 namespace Purse
 {
@@ -10,6 +11,8 @@ namespace Purse
     {
         static void Main(string[] args)
         {
+            Class1 money = new Class1(20);
+            money.Add(500);
         }
     }
 }
